c1c89dbe0296d264f028c2e80083e3ea33d09627
NItro <atadjanovdavronbek863@gmail.com>
2025-03-03 09:12:07 +0500

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,6 @@
 
 python manage.py migrate
 
-# Collect static files
-python manage.py collectstatic
-
 # Start server
-gunicorn core.wsgi:application --bind 0.0.0.0:8000
+#gunicorn core.wsgi:application --bind 0.0.0.0:8000
 exec "$@"
